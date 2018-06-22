f84f41f803e0e066f24cbf0e8991175e4b397ba8
YutaOikawa <oikenfight@gmail.com>
2018-06-22 22:26:41 +0900

    implement List component.

---
+++ resources/assets/js/components/List.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-10" style="padding-top: 40px;">
+                <!-- パンくず -->
+                <div class="row">
+                    <div class="col-sm-12">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item">
+                                <router-link :to="{ name: 'index' }">Home</router-link>
+                            </li>
+                            <li class="breadcrumb-item active">
+                                Date
+                            </li>
+                        </ol>
+                    </div>
+                </div>
+
+                <!-- タイトル -->
+                <div>
+                    <h2>スクリーンショット一覧</h2>
+                </div>
+
+                <!-- コンテンツ -->
+                <div class="row">
+                    <div class="col-12">
+                        <table class="table">
+                            <thead>
+                            <tr>
+                                <th class="col-sm-4">name</th>
+                                <th class="">image</th>
+                            </tr>
+                            </thead>
+                            <tbody v-for="image in images">
+                                <tr>
+                                    <td>
+                                        <router-link :to="{ name:'show', params:{date:date, image:image} }">{{ image }}</router-link>
+                                    </td>
+                                    <td>
+                                        <img :src="getSrc(image)" class="img-fluid" :alt="image">
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                images: [],
+                date: this.$route.params.date,
+            }
+        },
+        created() {
+            console.log(this.$route.params.date)
+            axios.get('/api/list/' + this.date)
+                .then(response => {
+                    this.images = response.data.images
+                })
+        },
+        methods: {
+            getSrc(image) {
+                return '/storage/' + this.date + '/' + image + '.png'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
