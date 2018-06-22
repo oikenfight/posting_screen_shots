3a86e8f4437ae6c0bd16d567722f6b034a693821
YutaOikawa <oikenfight@gmail.com>
2018-06-22 22:24:58 +0900

    fix index.blade.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,25 +14,9 @@
 
 {{-- Content --}}
 @section('content')
-    <div class="row">
-        <div class="col-sm-12">
-            <ol class="breadcrumb">
-                <li class="breadcrumb-item active">
-                    <a href="{{ route('index') }}">Home</a>
-                </li>
-            </ol>
+    <div>
+        <div id="app">
+            <router-view></router-view>
         </div>
-    </div>
-
-    <div>
-        <h2>日程一覧</h2>
-    </div>
-
-    <div>
-        <ul>
-            @foreach($dates as $date)
-                <li><a href="{{ route('list', $date) }}">{{ $date }}</a></li>
-            @endforeach
-        </ul>
     </div>
 @endsection
